3ead75706032871c6666b29404dc926b271ec933
Paulo Vieira <88544728+henrique509@users.noreply.github.com>
2021-08-10 17:18:24 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
-// Iteration 1: Names and Input
+let hacker1 = 'Paulo';
+let hacker2 = 'Adonis';
+const pilot = `The driver's name is ${hacker1}`;
+const nav = `The navigator's name is ${hacker2}`;
+console.log(pilot);
+console.log(nav)
 
 
-// Iteration 2: Conditionals
+let hacker1 = 'Paulo';
+let hacker2 = 'Adonis';
+
+const num1 = (hacker1.length);
+const num2 = (hacker2.length);
+
+if (num1 === num2) {
+  console.log(`Wow, you both have equally long names, ${num1} characters`)
+} else if (num1 > num2) {
+  console.log(`The driver has the longest name, it has ${num1} characters`)
+}  else if (num1 < num2) {
+    console.log(`It seems that the navigator has the longest name, it has ${num2} characters`)
+  }
 
 
 // Iteration 3: Loops
